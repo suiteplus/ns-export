8d15c9a0014fc3ae0395b754fa57f904dacf0363
danieljoppi <daniel.joppi@gmail.com>
2015-10-10 15:42:29 -0300

    - find all record types

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
 'use strict';
 var casper = require('casper').create({
-    // clientScripts:  [
-    //     'includes/jquery.js',      // These two scripts will be injected in remote
-    //     'includes/underscore.js'   // DOM on every request
-    //  ],
-    pageSettings: {
-        loadImages:  false,        // The WebPage instance used by Casper will
-        loadPlugins: false         // use these settings
-    },
-    logLevel: "info",              // Only "info" level messages will be logged
-    verbose: true                  // log messages will be printed out to the console
-});
+        pageSettings: {
+            loadImages:  false,        // The WebPage instance used by Casper will
+            loadPlugins: false         // use these settings
+        },
+        logLevel: "info",              // Only "info" level messages will be logged
+        verbose: true                  // log messages will be printed out to the console
+    }),
+    $ = require('./node_modules/jquery/dist/jquery.js');
 
 var fs = require('fs'),
     params = JSON.parse(fs.read('./nsconfig.json', 'utf-8'));
-//var nsconfig = require('nsconfig'),
-//    params = nsconfig({}, true);
 
+var base = params.realm || 'system.netsuite.com',
+    sysURL = 'https://'+base,
+    dotIdx = sysURL.indexOf('.'),
+    na1URL = sysURL.substring(0, dotIdx) + '.na1' + sysURL.substring(dotIdx);
 
 casper.start();
 
-/*Processo de Login*/
-/* faz o post direto na url do Netsuite*/
-casper.open('https://system.netsuite.com/app/login/nllogin.nl', {
+casper.open(sysURL + '/app/login/nllogin.nl', {
    method: 'post',
     data:   {
         'email' : params.email,
         'password' : params.password,
         'jsenabled': 'T'
     }
-});
-
-/* depois do post faz a validação da pergunta (2a validação do login)*/
-casper.then( function () {
-    this.viewport(1280,1024);
+}).then( function () {
+    this.viewport(1280, 1024);
     this.capture('ss/question.jpg');
 
     var html = this.getHTML().toString().toLowerCase();
     var quiz = params.quiz,
         has = false;
-    for (var i=0; i< quiz.length; i++) {
+    for (var i = 0; i < quiz.length; i++) {
         var q = quiz[i];
-        console.log('quiz', i, q.question, q.answer, !!~html.indexOf(q.question));
+        //console.log('quiz', i, q.question, q.answer, !!~html.indexOf(q.question));
         if (html.indexOf(q.question) >= 0) {
-            this.fillXPath('form', {'//input[@name="answer"]':q.answer},true);
+            this.fillXPath('form', {'//input[@name="answer"]': q.answer}, true);
             has = true;
         }
     }
@@ -53,19 +47,39 @@ casper.then( function () {
         casper.exit();
     }
 
-    casper.wait(15000);
+    casper.wait(15000, function() {
+        this.capture('ss/afterLogin.jpg');
 
-});
+        var html = this.getHTML().toString(),
+            $html = $(html),
+            rows = $html.find('tr.uir-list-row-tr');
 
-/* Após o login abre a tela de Lista de Customers */
-casper.then( function() {
-    this.capture('ss/afterLogin.jpg');
+        for (var r = 0; r < rows.length; r++) {
+            var row = $(rows[r]);
+            if (~row.text().indexOf(params.account)) {
+                this.click('span#' + $('span.checkbox_ck', row)[0].id);
+                break;
+            }
+        }
 
-    casper.open('https://system.na1.netsuite.com/app/common/entity/custjoblist.nl?Customer_STAGE=CUSTOMER&whence=');
+        casper.wait(1000, function () {
+            this.capture('ss/home.jpg');
 
-    casper.then( function() {
-        this.capture('ss/customer.jpg');
+            // load records type list
+            casper.open(na1URL + '/app/common/custom/custrecords.nl').then(function() {
+                this.capture('ss/record-list.jpg');
+                var html = this.getHTML().toString(),
+                    $html = $(html),
+                    rows = $html.find('tr.uir-list-row-tr');
+
+                for (var r = 0; r < rows.length; r++) {
+                    var row = $(rows[r]),
+                        recType = row.find('td:nth-child(3)').text().trim(),
+                        recId = row.find('td:nth-child(4)').text().trim();
+                    console.log(r, '->', recType, '[', recId, ']');
+                }
+            })
+        });
     });
 });
-
 casper.run();
